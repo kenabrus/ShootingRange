79b80ccaf59bf54a9521ea50f42736d16d962fe6
kenabrus <kenabrus@gmail.com>
2020-04-10 18:50:54 +0200

    dbcontext

--- ShootingRange.Server/Data/ApplicationDbContext.cs
+++ ShootingRange.Server/Data/ApplicationDbContext.cs
@@ -10,13 +10,10 @@ namespace ShootingRange.Server.Data
         }
 
         public DbSet<Contact> Contacts {get; set;}
-<<<<<<< HEAD
         public DbSet<Uzytkownik> Uzytkownicy {get; set;}
         public DbSet<Oferta> Oferty {get; set;}
         public DbSet<Samochod> Samochody {get; set;}
         public DbSet<Lokalizacja> Lokalizacje {get; set;}
-=======
->>>>>>> 22b4480249bd0ad61ca7e936bb48b9a1e2c5bd6c
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -25,14 +22,10 @@ namespace ShootingRange.Server.Data
             // Add your customizations after calling base.OnModelCreating(builder);
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Contact>().ToTable("Contact");
-<<<<<<< HEAD
             modelBuilder.Entity<Uzytkownik>().ToTable("Uzytkownik");
             modelBuilder.Entity<Oferta>().ToTable("Oferta");
             modelBuilder.Entity<Samochod>().ToTable("Samochod");
             modelBuilder.Entity<Lokalizacja>().ToTable("Lokalizacja");
-=======
-
->>>>>>> 22b4480249bd0ad61ca7e936bb48b9a1e2c5bd6c
         }
     }
 }
